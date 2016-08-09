0a5835cd7bf9527c2f70f4e4cc3c4eb572510550
Felipesl71 <felipeslima1@gmail.com>
2016-08-09 10:19:08 -0300

    Add files via upload
    Atualizações usando Struct

--- Projeto1P2.cpp
+++ Projeto1P2.cpp
@@ -1,7 +1,20 @@
 #include<stdio.h>
 
+struct pedido
+{
+    char nomePes[15];
+	char nomePed[30];
+    char CPF[15];
+    char numPed[10];
+    char qtdPed[50];
+    char valorped[200];
+    int cod;
+}TPedido[200];
+
+
 
 void cardapio(){
+	printf("Lanchonete do Alcides");
 	printf("\n Cachorro quente");
 	printf("\n Hanburguer");
 	printf("\n Coxinha");
@@ -9,6 +22,7 @@ void cardapio(){
 }
 
 void realizarpedido(){
+	
 	
 }
 
@@ -39,11 +53,21 @@ main(){
 		scanf("%d",&op);
 		if(op == 1){
 			cardapio();
-			
-			
 		}else if(op == 2){
+			}
+		else if(op == 3){
 			
-		}
+			}
+		else if(op == 4){
+			}
+		else if(op == 5){
+			}
+		else if(op == 6){
+			
+				printf("Creditos xxxx");
+				printf("Saindo do programa!");
+				
+			}	
 	}while(op != 6 || op>6 );
 	
 }
